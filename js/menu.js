f1187b6c8e6702e15f9e352ebf52f87f69cc4d4b
Peter Vágner <pvdeejay@gmail.com>
2025-10-26 08:19:16 +0100

    Menu button: add an alt attribute to the image and use aria-pressed to make screen reader users it's closed or open

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,18 @@
 function toggle_menu()
 {
   var obj = document.getElementById("menucontent");
+  var button = document.getElementById("toggle");
   if (!document.getElementById("menu").classList.contains("menu_open"))
   {
     document.getElementById("menu").classList.add("menu_open");
     document.getElementById("menu").classList.remove("menu_close");
+    button.setAttribute("aria-pressed", true);
   }
   else
   {
     document.getElementById("menu").classList.add("menu_close");
     document.getElementById("menu").classList.remove("menu_open");
+    button.setAttribute("aria-pressed", false);
   }
 }
 
